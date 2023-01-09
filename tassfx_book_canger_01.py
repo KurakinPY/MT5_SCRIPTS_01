a643b17e6a1c04bdfb698b99dbd942c232b0b14f
KurakinPY <info@kurakins.ru>
2023-01-09 15:36:58 +0300

    1

--- tassfx_book_canger_01.py
+++ tassfx_book_canger_01.py
@@ -1,2 +1,6 @@
-print("Hello World!")
-print("Hello World!")
+"""
+Скрипт меняет группу счета в зависимости от цвета счета при отсутствии открытых позиций
+if Color == Red and Group == bbook then Group = abook
+if Color == Green and Group == abook then Group = bbook
+"""
+
